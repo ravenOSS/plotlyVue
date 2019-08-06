29d16c84b9ff5e8e094781c3cbc559294aba3e57
ravenOSS <35054853+ravenOSS@users.noreply.github.com>
2019-08-05 18:06:39 -0600

    Get rid of HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
+    <BasicChart msg="This is the location for the basic chart"/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BasicChart from './components/BasicChart.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BasicChart,
   }
 }
 </script>
---
+++ src/components/BasicChart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="view">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicChart",
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="css" scoped>
+h1 {
+  color: red
+}
+</style>
+
+
